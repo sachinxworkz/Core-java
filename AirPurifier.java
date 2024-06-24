8173540c5b1f987b469f5fc8447bebe3f6bc59b7
demo <demo@Anands-MacBook-Air.local>
2024-06-24 11:15:08 +0530

    hgyj

---
+++ AirPurifier.java
@@ -0,0 +1,15 @@
+class AirPurifier {
+    static String brand = "Honeywell";
+    static String model = "HPA300";
+    static double price = 249.99;
+    static String color = "Black";
+    static String type = "HEPA";
+
+    public static void main(String[] args) {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: $" + price);
+        System.out.println("Color: " + color);
+        System.out.println("Type: " + type);
+    }
+}
