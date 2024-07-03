d136b79e6aa1ef9673349667bc8e721be8b69e7d
sachinxworkz <sachin2001ph@gmail.com>
2024-07-03 10:34:53 +0530

    gdtdutcutdyr

---
+++ Chair.java
@@ -0,0 +1,10 @@
+class CellPhone {
+
+
+public static void main(String[] motorol){
+
+    System.out.print("CellPhone is used for Communication. |n");
+    Ststem.out.print("Cellphone is essential thing in today's Life'")
+}
+
+}
