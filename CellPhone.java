26b6266239dbd0fc7e85e71e64a86af271ea96ef
sachinxworkz <sachin2001ph@gmail.com>
2024-07-03 11:04:46 +0530

    args

---
+++ CellPhone.java
@@ -0,0 +1,7 @@
+class CellPhone {
+public static void main(String[] iphone){
+
+System.out.println("CellPhone is used for Communication");
+System.out.println("CsllPhone is essentian for life");
+}
+}
