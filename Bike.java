3c1c2d7b51820025a7da0a98da61f4cb8bdbf243
sachinxworkz <sachin2001ph@gmail.com>
2024-07-04 10:24:32 +0530

    commiting basic related java codes

---
+++ Bike.java
@@ -0,0 +1,14 @@
+class Bike{
+
+static String name ;
+static int price ;
+
+public static void main(String [] bike){
+
+
+    System.out.println("the bike name is "+name);
+     System.out.println("the bike price is "+price);
+}
+
+
+}
---
+++ Edit1.java
@@ -0,0 +1,16 @@
+class Fruit{
+
+
+public static void main(String fruitBazar[]){
+String fruitName = "Banana" ;
+int price = 80 ;
+System.out.println("The Fruit Name is" + fruit name);
+System.out.println("The Fruit Priceis" + price);
+
+}
+
+
+
+
+
+}
